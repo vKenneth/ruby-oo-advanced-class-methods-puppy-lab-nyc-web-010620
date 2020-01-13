72e824ad76606fae0a71628ae9355ee438c9551f
Kenneth Valdivia <kenneth_valdivia@yahoo.com>
2020-01-13 03:52:54 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
-# Add your code here
+ require_relative "../lib/dog.rb"
+ 
+   class Dog
+  attr_accessor :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+ 
+  def self.create(name)
+    dog = self.new
+    dog.name = name
+    @@all << dog
+  end
+ 
+  def self.print_all
+    self.all.each{|person| puts "#{dog.name}"}
+  end
+end
+ 
+Dog.create("Pluto")
+Person.create("Fido")
+Dog.create("Maddy")
+ 
+Person.print_all
+   
