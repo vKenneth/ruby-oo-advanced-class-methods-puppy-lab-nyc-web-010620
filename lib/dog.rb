35c0bfae2c42b0781650d96b3363d6002f563d01
Kenneth Valdivia <kenneth_valdivia@yahoo.com>
2020-01-13 03:50:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -32,6 +32,29 @@ end
  def self.destroy_all
    self.all.clear
  end
+ binding.pry
+   class Dog
+  attr_accessor :name
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
  
-   
+  def self.create(name)
+    dog = self.new
+    dog.name = name
+    @@all << dog
+  end
+ 
+  def self.print_all
+    self.all.each{|person| puts "#{dog.name}"}
+  end
+end
+ 
+Dog.create("Pluto")
+Person.create("Fido")
+Dog.create("Maddy")
+ 
+Person.print_all
    
